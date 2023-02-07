8035d850be3666ff8e1b385e28548ce37a7669e6
billy6262 <80078196+billy6262@users.noreply.github.com>
2023-02-07 14:45:27 -0800

    Add files via upload

---
+++ test program.py
@@ -0,0 +1,23 @@
+c = 1
+a = 1
+b = 1
+sqrc = a ** 2 + b ** 2
+print("hello. the idea of this program is to find the area and perimiter of a triangel")             # into message
+print("to start off would you pleas enter the lenth of each side of your triangel one at a time.")
+print("pleas enter them in order of shotest lanth first ending in longest.")
+
+
+a = input()
+a = int(a)
+b = input()
+a = int(b)
+c = input()
+a = int(c)
+d = a * a + b * b
+d = int(d)
+area = d //  d
+per = a + b + c
+
+print("the area is       " + area)
+
+print("the perimiter is  " + per)
